b6572abed23f41a1476ba3521ff4b35fb7fc9c9c
Ajay3553 <ajaysingh.102007@gmail.com>
2025-08-23 23:59:04 +0530

    Added watch history endpoint and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { apiResponse } from '../utils/apiResponse.js';
 import {v2 as cloudinary} from 'cloudinary';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
 const options = {
     httpOnly: true,
@@ -382,6 +383,59 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
   )
 })
 
+const getWatchHistory = asyncHandler( async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "video",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  }
+                },
+                {
+                  $addFields: {
+                    owner: {
+                      $first: "$owner"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ])
+  return res
+  .status(200)
+  .json(
+    new apiResponse(
+      200,
+      user[0].watchHistory,
+      "Watch History fetch Successfully"
+    )
+  )
+})
+
 export {
   registerUser,
   loginUser,
@@ -392,5 +446,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   upadateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controllers/user.controller.js';
+import { changeCurrentPassword, getCurrectUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, upadateUserCoverImage, updateAccountDetails, updateUserAvatar } from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -24,5 +24,20 @@ router.route('/login').post(loginUser)
 // secured Routes
 router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrectUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)  //not using post bacuse if post is used then all data will be get changed that's why used patch only specific info will update
+router.route("/avatar").patch(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar
+)
+router.route("/cover-image").patch(
+    verifyJWT,
+    upload.single("/coverImage"),
+    upadateUserCoverImage
+)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) // since data is coming from params now c/: username will display the username of user in URL
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 export default router
